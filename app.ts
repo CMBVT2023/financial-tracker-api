b8a641971dd6ba9f641af59330a605fe0a3a5e4b
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-02-12 19:08:58 -0800

    Added a template env file

--- app.ts
+++ app.ts
@@ -7,10 +7,17 @@ const server = express();
 // Enables the default cors for the server
 server.use(cors());
 
+// Temp placeholder
+const port = 1000;
+
 // Parse all body data into json
 server.use(express.json())
 
 server.get('/test', (req, res) => {
     console.log('Connection made...')
     res.status(200).json(serverResponseObj(true, 'Connection made...', 'none'));
+})
+
+server.listen(port, () => {
+    console.log(`Sever listening on port https://localhost:${port}.`);
 })
